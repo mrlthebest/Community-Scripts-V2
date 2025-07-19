47fadcc831b29dcb1da1bbc181340d7c56bad5af
Marlon <115442027+mrlthebest@users.noreply.github.com>
2025-07-19 04:26:46 -0300

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,10 +1,12 @@
 data = {
     dbo = {
-        ['Combo'] = {
+        ['Actions'] = {
             description = {
-                {language = 'pt_br', text = ''},
+                {language = 'pt_br', text = 'Ola Mundo'},
+                {language = 'en_us', text = 'Hello World'},
             },
-            author = 'mrlthebest'
+            author = 'mrlthebest',
+            date = '10/10/2010'
         }
     }
 }
