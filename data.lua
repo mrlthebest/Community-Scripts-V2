77c9ee435748bee874192c143c8c91e00aa1e0ed
Marlon <115442027+mrlthebest@users.noreply.github.com>
2025-07-20 21:39:36 -0300

    Update data.lua

--- data.lua
+++ data.lua
@@ -5,6 +5,11 @@ data = {
                 {language = 'pt_br', text = 'Ola Mundo'},
                 {language = 'en_us', text = 'Hello World'},
             },
+            title = {
+                {language = 'pt_br', text = 'Actions.lua'},
+                {language = 'en_us', text = 'Actions.lua'},
+            },
+            tags = 'Cavebot, Actions, vBot',
             author = 'mrlthebest',
             date = '10/10/2010'
         }
